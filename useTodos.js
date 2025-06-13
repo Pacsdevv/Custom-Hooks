6db7df2c4a83467d0b2b1993d74283618d9a7075
Pablo Raldes <pablitoxdggg@gmail.com>
2025-06-13 01:28:26 -0400

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,44 @@
+import { useReducer, useEffect } from 'react';
+import { todoReducer } from '../08-useReducer/todoReducer';
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || [];
+};
+
+export const useTodos = (initialState = []) => {
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    dispatch({
+      type: 'Add Todo',
+      payload: todo,
+    });
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: 'Remove Todo',
+      payload: id,
+    });
+  };
+
+  const handleToggleTodo = (id) => {
+    dispatch({
+      type: 'Toggle Todo',
+      payload: id,
+    });
+  };
+
+  return {
+    todos,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+    todosCount: todos.length,
+    pendingTodosCount: todos.filter((todo) => !todo.done).length,
+  };
+};
